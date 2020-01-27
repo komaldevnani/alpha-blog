5555e33a303a179492a9c841ad8cd66035db5da1
komal devnani <komaldevnani00@gmail.com>
2020-01-27 23:14:26 +0530

    doing some front end styling

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,7 +11,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-      flash[:notice] = "Article was successfully created"
+      flash[:success] = "Article was successfully created"
       redirect_to article_path(@article)
     else
       render 'new'
@@ -26,7 +26,7 @@ class ArticlesController < ApplicationController
 
   def update
     if @article.update(article_params)
-      flash[:notice] = "Article was successfully edited"
+      flash[:success] = "Article was successfully edited"
       redirect_to article_path(@article)
     else
       render 'edit'
@@ -35,7 +35,7 @@ class ArticlesController < ApplicationController
 
   def destroy
     @article.destroy
-    flash[:notice] = "Article was sucessfully deleted"
+    flash[:danger] = "Article was sucessfully deleted"
     redirect_to articles_path
   end
   
