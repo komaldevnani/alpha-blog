4013e9d5ebc3691b392bb9f005e7c80d39b75052
komal devnani <komaldevnani00@gmail.com>
2020-01-30 21:55:24 +0530

    creating user table and validation

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ActiveRecord::Base
+  validates :username, presence: TRUE, length:{minimum: 3, maximum: 25},
+                        uniqueness: {case_sensitive: false}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\Z/i
+  validates :email, presence: TRUE, length: {maximum: 105}, 
+        uniqueness: {case_sensitive: false}, format: { with: VALID_EMAIL_REGEX}
+end
