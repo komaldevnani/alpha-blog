adb4a7fc89176b6f21a3104b0d700c1cc44a4567
komal devnani <komaldevnani00@gmail.com>
2020-02-06 15:04:23 +0530

    full authentication and authorization

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only:[:show, :edit, :update, :destroy]
+  before_action :require_user, except:[:index,:show]
+  before_action :require_same_user, only:[:edit,:update]
   def index
     @articles = Article.all 
   end
@@ -10,7 +12,7 @@ class ArticlesController < ApplicationController
   
   def create
     @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = current_user
     if @article.save
       flash[:success] = "Article was successfully created"
       redirect_to article_path(@article)
@@ -46,5 +48,11 @@ class ArticlesController < ApplicationController
   end
   def article_params
     params.require(:article).permit(:title,:description)
+  end
+  def require_same_user
+    if current_user != @article.user and !current_user.admin?
+      flash[:danger] = "You can only edit or delete own articles"
+      redirect_to root_path
+    end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
     before_action :set_user, only:[:show, :edit]
+    before_action :require_same_user, only:[:edit, :update]
+    before_action :require_admin, only:[:destroy]
     def index
         @users = User.all
     end
@@ -10,8 +12,9 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id
             flash[:success] = "Welcome to alpha blog #{@user.username}"
-            redirect_to articles_path
+            redirect_to user_path(@user)
         else
             render 'new'
         end
@@ -31,11 +34,28 @@ class UsersController < ApplicationController
             render 'edit'
         end
     end
+    def destroy
+        @user.destroy
+        flash[:danger] = "user and all articles created by user have been deleted"
+        redirect_to users_path
+    end
     private
     def user_params
         params.require(:user).permit(:username,:email,:password)
     end
     def set_user
         @user= User.find(params[:id])
+    end
+    def require_same_user
+        if current_user != @user and !current_user.admin?
+            flash[:danger] = "You can only edit your own articles"
+            redirect_to root_path
+        end
+    end
+    def require_admin
+        if logged_in? and !current_user.admin?
+            flash[:danger] = "Only admin can perform this action"
+            redirect_to root_path
+        end
     end
 end
